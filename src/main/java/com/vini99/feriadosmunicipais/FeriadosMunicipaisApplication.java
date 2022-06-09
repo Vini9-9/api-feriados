0416049592a0588167abcbcbf0e49845e578a88e
Vinicius Pessoa <vini.pessoa99@gmail.com>
2022-06-09 13:28:16 -0300

    first commit

---
+++ src/main/java/com/vini99/feriadosmunicipais/FeriadosMunicipaisApplication.java
@@ -0,0 +1,13 @@
+package com.vini99.feriadosmunicipais;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeriadosMunicipaisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeriadosMunicipaisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vini99/feriadosmunicipais/FeriadosMunicipaisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vini99.feriadosmunicipais;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeriadosMunicipaisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
